74f66f430f47d40b75facb10b3a2dce8d68dc176
proskurko@gmail.com <proskurko@gmail.com>
2025-03-22 03:08:07 +0200

    add: file with settings for hydration

---
+++ tests/utils/hydrationSettings.ts
@@ -0,0 +1,9 @@
+export const settingsHard = {
+  intervals: [1_000, 2_000, 10_000],
+  timeout: 60_000,
+};
+
+export const settingsLight = {
+  intervals: [500, 1000, 1500],
+  timeout: 15_000,
+};
