8a40131f4641e1a776e9c30f2302342c7908aec3
proskurko@gmail.com <proskurko@gmail.com>
2025-03-22 04:27:56 +0200

    update: hydration optimization

--- tests/utils/hydrationSettings.ts
+++ tests/utils/hydrationSettings.ts
@@ -4,6 +4,8 @@ export const settingsHard = {
 };
 
 export const settingsLight = {
-  intervals: [500, 1000, 1500],
-  timeout: 15_000,
+  // intervals: [500, 1000, 1500],
+  // timeout: 15_000,
+  intervals: [1_000, 2_000, 10_000],
+  timeout: 60_000,
 };
